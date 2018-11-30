1eeac2ab93a3c8578046fd9abf6d33bbdfa938f0
Ben Smith <bh2smith@gmail.com>
2018-11-30 13:53:22 +0100

    - Separate unit tests into Serial and Distributed. - JSON import now accepts string or long values in constraint dictionary.

---
+++ src/test/java/relations/DistributedInputFeedTest.java
@@ -0,0 +1,62 @@
+package relations;
+
+import algebra.curves.barreto_naehrig.bn254a.*;
+import configuration.Configuration;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import relations.objects.Assignment;
+import relations.r1cs.FileToR1CS;
+
+import relations.r1cs.R1CSRelationRDD;
+import scala.Tuple3;
+import java.io.Serializable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DistributedInputFeedTest implements Serializable {
+    private transient JavaSparkContext sc;
+    private Configuration config;
+    private Tuple3<R1CSRelationRDD<BN254aFields.BN254aFr>,
+            Assignment<BN254aFields.BN254aFr>,
+            JavaPairRDD<Long, BN254aFields.BN254aFr>> distributedFromJSON;
+    private String jsonFilePath;
+    private String textFilePath;
+
+
+    @Before
+    public void setUp() {
+        sc = new JavaSparkContext("local", "ZKSparkTestSuite");
+        config = new Configuration(2, 2, 1, 4, sc, StorageLevel.MEMORY_ONLY());
+
+        jsonFilePath = "src/test/data/json/";
+        textFilePath = "src/test/data/text/";
+
+        distributedFromJSON = FileToR1CS.distributedR1CSFromJSON(jsonFilePath + "pepper_out.json", config);
+//        distributedFromFileExample = FileToR1CS.distributedR1CSFromPlainText(textFilePath);
+
+
+    }
+
+    @After
+    public void tearDown() {
+        sc.stop();
+        sc = null;
+    }
+
+    @Test
+    public void distributedR1CSFromJSONTest() {
+
+        assertTrue(distributedFromJSON._1().isValid());
+
+        assertTrue(distributedFromJSON._1().isSatisfied(distributedFromJSON._2(), distributedFromJSON._3()));
+    }
+
+
+
+
+}
---
+++ src/test/java/relations/SerialInputFeedTest.java
@@ -0,0 +1,96 @@
+package relations;
+
+import algebra.curves.barreto_naehrig.bn254a.*;
+import algebra.curves.barreto_naehrig.bn254a.bn254a_parameters.BN254aG1Parameters;
+import algebra.curves.barreto_naehrig.bn254a.bn254a_parameters.BN254aG2Parameters;
+import configuration.Configuration;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import relations.objects.Assignment;
+import relations.r1cs.FileToR1CS;
+import relations.r1cs.R1CSRelation;
+import scala.Tuple3;
+import zk_proof_systems.zkSNARK.SerialProver;
+import zk_proof_systems.zkSNARK.SerialSetup;
+import zk_proof_systems.zkSNARK.Verifier;
+import zk_proof_systems.zkSNARK.objects.CRS;
+import zk_proof_systems.zkSNARK.objects.Proof;
+
+import java.io.Serializable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SerialInputFeedTest implements Serializable {
+    private transient JavaSparkContext sc;
+    private Configuration config;
+    private Tuple3<R1CSRelation<BN254aFields.BN254aFr>,
+            Assignment<BN254aFields.BN254aFr>,
+            Assignment<BN254aFields.BN254aFr>> r1csFromJSON;
+    private R1CSRelation<BN254aFields.BN254aFr> r1csFromText;
+    private BN254aFields.BN254aFr fieldFactory;
+    private BN254aG1 g1Factory;
+    private BN254aG2 g2Factory;
+    private R1CSRelation<BN254aFields.BN254aFr> r1cs;
+    private Assignment<BN254aFields.BN254aFr> primary;
+    private Assignment<BN254aFields.BN254aFr> auxiliary;
+    private BN254aPairing pairing;
+    private CRS<BN254aFields.BN254aFr, BN254aG1, BN254aG2, BN254aGT> CRS;
+    private Proof<BN254aG1, BN254aG2> proof;
+    private String jsonFilePath;
+    private String textFilePath;
+
+
+    @Before
+    public void setUp() {
+        // Dummy Configuration
+        config = new Configuration();
+
+        jsonFilePath = "src/test/data/json/";
+        textFilePath = "src/test/data/text/";
+
+        r1csFromJSON = FileToR1CS.serialR1CSFromJSON(jsonFilePath + "satisfiable_pepper.json");
+
+        // Notice that FromText returns only the constraints.
+        r1csFromText = FileToR1CS.serialR1CSFromPlainText(textFilePath);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void serialR1CSFromJSONTest() {
+        assertTrue(r1csFromJSON._1().isValid());
+        assertTrue(r1csFromJSON._1().isSatisfied(r1csFromJSON._2(), r1csFromJSON._3()));
+    }
+
+
+    @Test
+    public void serialR1CSFromTextTest() {
+        assertTrue(r1csFromText.isValid());
+
+    }
+
+    @Test
+    public void serialCRSTest() {
+        // TODO - clean this up.
+        fieldFactory = new BN254aFields.BN254aFr(2L);
+        g1Factory = new BN254aG1Parameters().ONE();
+        g2Factory = new BN254aG2Parameters().ONE();
+        pairing = new BN254aPairing();
+
+        r1cs = r1csFromJSON._1();
+        primary = r1csFromJSON._2();
+        auxiliary = r1csFromJSON._3();
+
+        CRS = SerialSetup.generate(r1cs, fieldFactory, g1Factory, g2Factory, pairing, config);
+
+        proof = SerialProver.prove(CRS.provingKey(), primary, auxiliary, fieldFactory, config);
+
+        assertTrue(Verifier.verify(CRS.verificationKey(), primary, proof, pairing, config));
+    }
+
+}
