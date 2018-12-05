e10727a0ee916005f27395db364ed64c0227c81b
Ben Smith <bh2smith@gmail.com>
2018-12-05 12:03:37 +0100

    [Milestone] First working Distributed input from Text

--- src/main/java/input_feed/distributed/TextToDistributedR1CS.java
+++ src/main/java/input_feed/distributed/TextToDistributedR1CS.java
@@ -14,6 +14,7 @@ import scala.Tuple2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<FieldT>>
     extends abstractFileToDistributedR1CS {
@@ -22,11 +23,11 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
         super(_filePath, _config);
     }
 
-    public R1CSRelationRDD loadR1CS(String fileName) {
+    public R1CSRelationRDD<FieldT> loadR1CS(String fileName) {
         String[] constraintParameters = new String[3];
         try{
             constraintParameters = new BufferedReader(
-                    new FileReader(this.filePath() + ".problem_size")).readLine().split(" ");
+                    new FileReader(this.filePath() + fileName + ".problem_size")).readLine().split(" ");
 
         } catch (Exception e){
             System.err.println("Error: " + e.getMessage());
@@ -41,13 +42,13 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
         final ArrayList<Integer> partitions = constructPartitionArray(this.config().numPartitions(), numConstraints);
 
         JavaPairRDD<Long, LinearTerm<FieldT>> linearCombinationA = distributedCombination(
-                this.filePath() + ".a", partitions, numConstraints);
+                this.filePath() + fileName + ".a", partitions, numConstraints);
 
         JavaPairRDD<Long, LinearTerm<FieldT>> linearCombinationB = distributedCombination(
-                this.filePath() + ".b", partitions, numConstraints);
+                this.filePath() + fileName + ".b", partitions, numConstraints);
 
         JavaPairRDD<Long, LinearTerm<FieldT>> linearCombinationC = distributedCombination(
-                this.filePath() + ".c", partitions, numConstraints);
+                this.filePath() + fileName + ".c", partitions, numConstraints);
 
         linearCombinationA.count();
         linearCombinationB.count();
@@ -63,13 +64,70 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
     }
 
     @Override
-    public Tuple2<Assignment, JavaPairRDD> loadWitness(String fileName) {
-        // TODO - not urgent
-        return null;
+    public Tuple2<Assignment<FieldT>, JavaPairRDD<Long, FieldT>> loadWitness(String fileName) {
+
+        final Assignment<FieldT> serialAssignment = new Assignment<>();
+        int numInputs = -1;
+        int numAuxiliary = -1;
+
+        try{
+            String[] constraintParameters = new BufferedReader(
+                    new FileReader(this.filePath() + fileName + ".problem_size")).readLine().split(" ");
+
+            numInputs = Integer.parseInt(constraintParameters[0]);
+            numAuxiliary = Integer.parseInt(constraintParameters[1]);
+
+            BufferedReader br = new BufferedReader(
+                    new FileReader(this.filePath() + fileName + ".witness"));
+
+            String nextLine;
+            int count = 0;
+            while ((nextLine = br.readLine()) != null) {
+                final BN254aFields.BN254aFr value = new BN254aFields.BN254aFr(nextLine);
+                serialAssignment.add((FieldT) value);
+                count++;
+            }
+            assert (count == numInputs + numAuxiliary);
+            br.close();
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+
+
+        int totalSize = numInputs + numAuxiliary;
+
+        final int numExecutors = this.config().numExecutors();
+        final ArrayList<Integer> assignmentPartitions = new ArrayList<>();
+        for (int i = 0; i < numExecutors; i++) {
+            assignmentPartitions.add(i);
+        }
+        if (totalSize % 2 != 0) {
+            assignmentPartitions.add(numExecutors);
+        }
+
+        JavaPairRDD<Long, FieldT> distributedAssignment = this.config().sparkContext()
+                .parallelize(assignmentPartitions, numExecutors).flatMapToPair(part -> {
+                    final long startIndex = part * (totalSize / numExecutors);
+                    final long partSize = part == numExecutors ?
+                            totalSize % (totalSize / numExecutors) : totalSize / numExecutors;
+
+                    final ArrayList<Tuple2<Long, FieldT>> assignment = new ArrayList<>();
+                    for (long i = startIndex; i < startIndex + partSize; i++) {
+                        assignment.add(new Tuple2<>(i, serialAssignment.get((int) i)));
+                    }
+                    return assignment.iterator();
+                }).persist(this.config().storageLevel());
+
+
+        final Assignment<FieldT> primary = new Assignment<>(serialAssignment.subList(0, numInputs));
+
+
+        return new Tuple2<>(primary, distributedAssignment);
     }
 
-    private <FieldT extends AbstractFieldElementExpanded<FieldT>>
-    JavaPairRDD<Long, LinearTerm<FieldT>>
+
+
+    private JavaPairRDD<Long, LinearTerm<FieldT>>
     distributedCombination(
             String fileName,
             ArrayList<Integer> partitions,
@@ -82,8 +140,10 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
 
         JavaPairRDD<Long, LinearTerm<FieldT>> result;
         try {
+
             final BufferedReader br = new BufferedReader(new FileReader(fileName));
-            // Not gonna work. Buffered reader gets reopened for each partition. May need to partition the file.
+            // TODO - this is not the best for performance
+            ArrayList<LinearCombination<FieldT>> constraintArray = serializeReader(br, numConstraints);
 
             result = this.config().sparkContext().parallelize(partitions, numPartitions).flatMapToPair(part -> {
                 final long partSize = part == numPartitions ? numConstraints %
@@ -93,10 +153,9 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
                 for (long i = 0; i < partSize; i++) {
 
                     final long index = part * (numConstraints / numPartitions) + i;
+                    LinearCombination<FieldT> currRow = constraintArray.get((int) index);
 
-                    LinearCombination<FieldT> combinationA = makeRowAt(index, br);
-
-                    for (LinearTerm term: combinationA.terms()) {
+                    for (LinearTerm term: currRow.terms()) {
                         T.add(new Tuple2<>(index, term));
                     }
 
@@ -138,5 +197,33 @@ public class TextToDistributedR1CS<FieldT extends AbstractFieldElementExpanded<F
             System.err.println("Error: " + e.getMessage());
         }
         return L;
+    }
+
+    public <FieldT extends AbstractFieldElementExpanded<FieldT>> ArrayList<LinearCombination<FieldT>>
+    serializeReader(BufferedReader br, int numConstraints) {
+        int index = 0;
+        ArrayList<LinearCombination<FieldT>> constraintList = new ArrayList<>();
+        try {
+            String nextLine;
+            while ((nextLine = br.readLine()) != null) {
+                String[] tokens = nextLine.split(" ");
+                int col = Integer.parseInt(tokens[0]);
+                int row = Integer.parseInt(tokens[1]);
+                BN254aFields.BN254aFr value = new BN254aFields.BN254aFr(tokens[2]);
+
+                final LinearCombination<FieldT> L = new LinearCombination<>();
+                if (row == index) {
+                    L.add(new LinearTerm<>(col, (FieldT) value));
+                    br.mark(100);
+                } else if (row > index) {
+                    constraintList.add(index, L);
+                    br.reset();
+                    index++;
+                }
+            }
+        } catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        return constraintList;
     }
 }
