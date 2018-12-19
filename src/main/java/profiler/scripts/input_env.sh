93b8b9f9f1fd2a160626f569a2bfd8eccdf3f214
Ben Smith <bh2smith@gmail.com>
2018-12-19 12:45:05 +0100

    generalized input profiler scripts

---
+++ src/main/java/profiler/scripts/input_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export APP_TYPE='distributed'
+export APP='input-feed'
+export FILE_PATH="/data/hash_2M/hash_2M"
+export MEMORY=13G
+export SIZE=20
+export CORES=2
+export NUM_EXECUTORS=20
+export NUM_PARTITIONS=10
